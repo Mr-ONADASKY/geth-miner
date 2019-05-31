31c9694c0f287d8c72459f12a76f36c3adacfa5f
Nick Vanden Eynde <nvde1960@gmail.com>
2019-05-31 18:45:35 +0200

    disable mining on start

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@ fi
 chmod +x ./geth
 ./geth init genesis.json --datadir "/data"
 ./geth account import --password ~/.accountpassword --datadir "/data" ~/.privatekey
-exec ./geth --datadir "/data" --bootnodes "enode://$bootnodeId@$bootnodeIp:30301" --networkid="6969" --verbosity=4 --gasprice '0' --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --mine --etherbase $address --unlock $address --password ~/.accountpassword --rpcapi="admin,clique,db,eth,debug,miner,net,shh,txpool,personal,web3" --mine
+exec ./geth --datadir "/data" --bootnodes "enode://$bootnodeId@$bootnodeIp:30301" --networkid="6969" --verbosity=4 --gasprice '0' --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --mine --etherbase $address --unlock $address --password ~/.accountpassword --rpcapi="admin,clique,db,eth,debug,miner,net,shh,txpool,personal,web3"
