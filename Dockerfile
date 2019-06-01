25c7df43647be36bee34124e7925de01383a6c59
Nick Vanden Eynde <nvde1960@gmail.com>
2019-06-01 12:09:06 +0200

    add more env options

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,13 @@ LABEL Nick Vanden Eynde ninjawulf98@gmail.com
 VOLUME ["/data", "/genesis"]
 
 ENV binary=geth-alltools-linux-amd64-1.8.27-4bcc0a37.tar.gz \
-  gethBaseUrl=https://gethstore.blob.core.windows.net/builds
+  gethBaseUrl=https://gethstore.blob.core.windows.net/builds \
+  bootnodeport=30301 \
+  syncmode=full \
+  networkid=6969 \
+  gasprice=0 \
+  gcmode=archive \
+  rpcapi=admin,clique,db,eth,debug,miner,net,shh,txpool,personal,web3
 
 RUN apt-get update \
   && apt-get install -y wget \
--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@ fi
 chmod +x ./geth
 ./geth init genesis.json --datadir "/data"
 ./geth account import --password ~/.accountpassword --datadir "/data" ~/.privatekey
-exec ./geth --datadir "/data" --bootnodes "enode://$bootnodeId@$bootnodeIp:30301" --networkid="6969" --verbosity=4 --gasprice '0' --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --etherbase $address --unlock $address --password ~/.accountpassword --rpcapi="admin,clique,db,eth,debug,miner,net,shh,txpool,personal,web3" --syncmode "full"
+exec ./geth --datadir "/data" --bootnodes "enode://$bootnodeId@$bootnodeIp:$bootnodeport" --networkid=$networkid --verbosity=4 --gasprice $gasprice --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --etherbase $address --unlock $address --password ~/.accountpassword --rpcapi=$rpcapi --syncmode $syncmode --gcmode $gcmode
