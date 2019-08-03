7089cb50aac21dfc3bbe7d8b1bd29a8bbb9e8f22
ninjawulf98 <ninjawulf98@gmail.com>
2019-08-03 15:45:21 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,14 +4,10 @@ wget "$gethBaseUrl/$binary"
 tar -xzvf $binary --strip 1
 rm $binary
 
-echo $password > ~/.accountpassword
-echo $privatekey > ~/.privatekey
-
 if test -f "/genesis/genesis.json"; then
   cp /genesis/genesis.json ./genesis.json
 fi
 
 chmod +x ./geth
 ./geth init genesis.json --datadir "/data"
-./geth account import --password ~/.accountpassword --datadir "/data" ~/.privatekey
-exec ./geth --datadir "/data" --bootnodes "enode://$bootnodeId@$bootnodeIp:$bootnodeport" --networkid=$networkid --verbosity=4 --gasprice $gasprice --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --etherbase $address --unlock $address --password ~/.accountpassword --rpcapi=$rpcapi --syncmode $syncmode --gcmode $gcmode
+exec ./geth --datadir "/data" --bootnodes "enode://$bootnodeId@$bootnodeIp:$bootnodeport" --networkid=$networkid --verbosity=4 --gasprice $gasprice --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --rpcapi=$rpcapi --syncmode $syncmode --gcmode $gcmode --rpcvhosts $rpcvhosts
