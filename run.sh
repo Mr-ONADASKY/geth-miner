6ca1fb3f1b22588dc927432bfdd4adf61b317879
ninjawulf98 <nvde1960@gmail.com>
2019-11-12 18:34:52 +0100

    allow insecure unlock

--- run.sh
+++ run.sh
@@ -14,4 +14,4 @@ fi
 chmod +x ./geth
 ./geth init genesis.json --datadir "/data"
 ./geth account import --password ~/.accountpassword --datadir "/data" ~/.privatekey
-exec ./geth --datadir "/data" --bootnodes "enode://$bootnodeId@$bootnodeIp:$bootnodeport" --networkid=$networkid --verbosity=4 --gasprice $gasprice --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --etherbase $address --unlock $address --password ~/.accountpassword --rpcapi=$rpcapi --syncmode $syncmode --gcmode $gcmode
+exec ./geth --datadir "/data" --bootnodes "enode://$bootnodeId@$bootnodeIp:$bootnodeport" --networkid=$networkid --verbosity=4 --gasprice $gasprice --rpc --rpcaddr "0.0.0.0" --rpccorsdomain "*" --etherbase $address --unlock $address --password ~/.accountpassword --rpcapi=$rpcapi --syncmode $syncmode --gcmode $gcmode --allow-insecure-unlock
